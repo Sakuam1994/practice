b40ee31a644e9a5060c75435afa36a6ae170c4fd
Sakuam1994 <s11098846@ymail.ne.jp>
2025-08-22 12:58:50 +0900

    Create TangibleAseset.java

---
+++ TangibleAseset.java
@@ -0,0 +1,15 @@
+public  class TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+
+public TangibleAsset(String name,int price,String color){
+  this.name=name;
+  this price=price;
+  this color=color;
+}  
+
+public String GetName(){return this.name}
+public int price(){return this.price}
+public String color(){return this.color}
+}
